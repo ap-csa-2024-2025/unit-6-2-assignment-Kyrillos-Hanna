5709250715a35bbc2eea84367644f75beaaa6cab
Kyrillos-Hanna <kyrohanna90@gmail.com>
2025-01-17 11:22:20 -0800

    finished hw

--- Main.java
+++ Main.java
@@ -6,21 +6,68 @@ public class Main
   {
     // Problem 1
     Scanner sc = new Scanner(System.in);
-    System.out.println("Enter array length");
+    System.out.println("Enter array length:");
     int len = sc.nextInt();
 
-    int[] arr = new int[len];
-    System.out.println("Enter values");
+    double[] arr = new double[len];
+    System.out.println("Enter values:");
     for (int i = 0; i < len; i++)
     {
-      arr[i] = sc.nextInt();
+      arr[i] = sc.nextDouble();
     }
-    System.out.println("Contains negative: " + containsNeg(arr));
+    System.out.println("Contains negative: " + containsNeg(arr) + "\n");
+
+    //Problem 2
+    System.out.println("Enter array length:");
+    int len2 = sc.nextInt();
+    int arr2[] = new int[len2];
+    System.out.println("Enter values:");
+    for (int i = 0; i < len2; i++) {
+      arr2[i] = sc.nextInt();
+    }
+    System.out.println("Number divisible by three: " + numDivisibleBy3(arr2) + "\n");
+
+    //Problem 3
+    System.out.println("Enter a value for num:");
+    int num = sc.nextInt();
+    System.out.println("Enter array length:");
+    int len3 = sc.nextInt();
+    System.out.println("Enter values:");
+    int arr3[] = new int[len3];
+    for (int i = 0; i < len3; i++) {
+      arr3[i] = sc.nextInt();
+    }
+    System.out.println("Num divisible by " + num + ": " + numDivisible(arr3, num));
+
   }
 
-  public static boolean containsNeg(int[] array)
+  public static boolean containsNeg(double[] array)
   {
-    // Implement Problem 1 here
-    return 0;
+    for(int i = 0; i < array.length; i++) {
+      if (array[i] < 0) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static int numDivisibleBy3(int[] array) {
+    int count = 0;
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] % 3 == 0) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  public static int numDivisible(int[] array, int num) {
+    int count = 0;
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] % num == 0) {
+        count++;
+      }
+    }
+    return count;
   }
 }
